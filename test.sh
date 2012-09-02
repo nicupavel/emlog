93a0d3c7c3f2d2d49341732a4d3f2944b1567ac4
Andreas Neustifter <andreas.neustifter@gmail.com>
2012-09-02 16:43:19 +0200

    Added test script.

--- test.sh
+++ test.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+echo ---- making emlog and nbcat
+make emlog.ko
+make nbcat
+echo ---- inserting module
 insmod emlog.ko
+echo ---- testing module
+mknod testlog c 250 8
+echo "testing testlog."$RANDOM > testlog
+./nbcat testlog
+rm testlog
+echo ---- removing module
 rmmod emlog
+echo ---- dmesg out
 dmesg | tail
